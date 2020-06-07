fda7018fd574e9e11642e8b700cad99d0b48f846
odattyan <62612364+odattyan@users.noreply.github.com>
2020-06-07 17:15:38 +0900

    haml change

--- db/migrate/20200607063229_devise_create_users.rb
+++ db/migrate/20200607063229_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname            null: false, unique: true
+      t.string :nickname,            null: false, unique: true
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :avatar
