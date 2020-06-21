1d3c2549dd5d8f65c05ddf6444373be713733538
odattyan <62612364+odattyan@users.noreply.github.com>
2020-06-21 19:40:02 +0900

    password_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          
+        #  validates :nickname, :family_name, :first_name, :family_name_reading, :first_name_reading, :birth_day, presence: true
+         has_one :card
+         has_many :items
 end
