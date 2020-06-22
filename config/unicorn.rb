d177eef60e36fa1a48752ee6384bf07cd06d3fad
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-06-22 20:40:38 +0900

    Nginxの導入

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ working_directory app_path
 
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 
