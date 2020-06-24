1f352b792ecb35a3c39f8df5c7bad6cbeb3b40c9
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-06-24 14:58:32 +0900

    Capistranoの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "items#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
+  # root "items#new"
+  resources :items, only: [:new]
 end
