8f1953f700a4b39f5b9595a8fc1d1b5e4ea7d31b
UmiUmiGame <201apamafua@gmail.com>
2020-06-28 21:16:02 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get 'items', to: 'show#index'
+  root to: 'items#index'
+  resources :items, only: [:show]
+
 end
