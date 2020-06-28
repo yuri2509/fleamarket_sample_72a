13fdfbfd7d1df42a84db06dc6b0706e142ca56ea
odattyan <62612364+odattyan@users.noreply.github.com>
2020-06-28 14:58:30 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200607063229) do
+ActiveRecord::Schema.define(version: 20200607080629) do
+
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.integer  "integer"
+    t.text     "description", limit: 65535
+    t.string   "status"
+    t.text     "judgment",    limit: 65535
+    t.integer  "size"
+    t.string   "cost"
+    t.string   "days"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "nickname",                                          null: false
@@ -30,19 +43,6 @@ ActiveRecord::Schema.define(version: 20200607063229) do
     t.datetime "updated_at",                                        null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-ActiveRecord::Schema.define(version: 20200607080629) do
-
-  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
-    t.integer  "integer"
-    t.text     "description", limit: 65535
-    t.string   "status"
-    t.text     "judgment",    limit: 65535
-    t.integer  "size"
-    t.string   "cost"
-    t.string   "days"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
   end
 
 end
