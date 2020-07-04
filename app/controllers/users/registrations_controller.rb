1125b5562c833b33d3289e7cf991f6008fabdf5f
odattyan <62612364+odattyan@users.noreply.github.com>
2020-07-04 23:38:01 +0900

    application.rb追記

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -46,7 +46,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
