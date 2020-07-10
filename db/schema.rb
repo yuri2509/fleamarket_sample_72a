502b6d6831f77fafec3213054c067c1666ffa727
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-07-10 22:49:04 +0900

    ancestryの導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200628105045) do
+ActiveRecord::Schema.define(version: 20200710070059) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "image",      null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+women = Category.create(:name=>"レディース")
+men = Category.create(:name=>"メンズ")
+baby = Category.create(:name=>"ベビー・キッズ")
+interior = Category.create(:name=>"インテリア・住まい・小物")
+book = Category.create(:name=>"本・音楽・ゲーム")
+toy = Category.create(:name=>"おもちゃ・ホビー・グッズ")
+cosmetic = Category.create(:name=>"コスメ・香水・美容")
+home-appliance = Category.create(:name=>"家電・スマホ・カメラ")
+sports = Category.create(:name=>"スポーツ・レジャー")
+handmade = Category.create(:name=>"ハンドメイド")
+ticket = Category.create(:name=>"チケット")
+car = Category.create(:name=>"自動車・オートバイ")
+other = Category.create(:name=>"その他")
+
+
+
+women_tops = women.children.create(:name=>"トップス")
+women_jacket = women.children.create(:name=>"ジャケット/アウター")
+women_pants = women.children.create(:name=>"パンツ")
+women_one-piece = women.children.create(:name=>"ワンピース")
+women_shoes = women.children.create(:name=>"靴")
+women_room-wear = women.children.create(:name=>"ルームウェア/パジャマ")
+women_leg-wear = women.children.create(:name=>"レッグウェア")
+women_hat = women.children.create(:name=>"帽子")
+women_bag = women.children.create(:name=>"バッグ")
+women_accessory = women.children.create(:name=>"アクセサリー")
+women_hair-accessory = women.children.create(:name=>"ヘアアクセサリー")
+women_small-articl = women.children.create(:name=>"小物")
+women_clock = women.children.create(:name=>"時計")
+women_wig = women.children.create(:name=>"ウィッグ/エクステ")
+women_yukata = women.children.create(:name=>"浴衣/水着")
+women_suit = women.children.create(:name=>"スーツ/フォーマル/ドレス")
+women_maternity = women.children.create(:name=>"マタニティ")
+women_other = women.children.create(:name=>"その他")
