61e7daaf344b787e7bfb576f77c7d059948934d5
odattyan <62612364+odattyan@users.noreply.github.com>
2020-07-11 20:15:46 +0900

    user.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
+
 describe User do
   describe '#create' do
-    it 
+    it "全て入力すれば登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it ""
