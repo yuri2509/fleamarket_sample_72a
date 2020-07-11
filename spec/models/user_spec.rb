de600e49ae2ac8fdedcc52aadde6c5ce6f5873d7
odattyan <62612364+odattyan@users.noreply.github.com>
2020-07-12 00:36:42 +0900

    実装しない為に削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe User do
   describe '#create' do
-    it "全て入力すれば登録できること" do
-      user = build(:user)
-      expect(user).to be_valid
-    end
+    context "有効なテスト" do
+      it "全て入力すれば登録できること" do
+        user = build(:user)
+        expect(user).to be_valid
+      end
 
-    it ""
+      it ""
