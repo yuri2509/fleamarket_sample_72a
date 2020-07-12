46edb9799cff7719e43f78f49675a31405bc814a
odattyan <62612364+odattyan@users.noreply.github.com>
2020-07-12 14:54:04 +0900

    全角のvalid（正規表現）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, password_length: 7..128
 
-         validates :nickname, :family_name, :first_name, :family_name_reading, :first_name_reading, :birth_day, presence: true
+         validates :nickname, :birth_day, :family_name, :first_name, :family_name_reading, :first_name_reading, presence: true
+         validates :family_name,
+          format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
+         validates :first_name,
+          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
+         validates :family_name_kana,
+          format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
+         validates :first_name_kana,
+          format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
+          
          has_one :card
          has_many :items
          has_one :destination
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,58 @@ describe User do
         expect(user).to be_valid
       end
 
-      it ""
+    context "無効なテスト" do
+      it "ニックネームの未入力" do
+        user = build(:user, nickname: nil)
+        user.valid?
+        expect(user.errors[:nickname]).to include('を入力してください')
+      end
+
+      it 'メールアドレスの未入力' do
+        user = build(:user, email: nil)
+        user.valid?
+        expect(user.errors[:email]).to include('を入力してください')
+      end
+
+      it 'メールアドレスの重複' do
+        user = create(:user)
+        another_user = build(:user, email: user.email)
+        another_user.valid?
+        expect(another_user.errors[:email]).to include('はすでに存在します')
+      end
+
+      it 'パスワードの未入力' do
+        user = build(:user, password: nil)
+        user.valid?
+        expect(user.errors[:password]).to include('を入力してください')
+      end
+
+      it '6文字以下のパスワード' do
+        user = build(:user, password: '000000', password_confirmation: '000000')
+        user.valid?
+        expect(user.errors[:password]).to include('は7文字以上で入力してください')
+      end
+
+      it '確認用パスワードの未入力' do
+        user = build(:user, password_confirmation: nil)
+        user.valid?
+        expect(user.errors[:password_confirmation]).to include('を入力してください')
+      end
+
+      it '確認用パスワードの不一致' do
+        user = build(:user, password_confirmation: "00000000")
+        user.valid?
+        expect(user.errors[:password_confirmation]).to include('とパスワードの入力が一致しません')
+      end
+
+      it 'ユーザー本名(苗字)の未入力' do
+        user = build(:user, family_name: nil)
+        user.valid?
+        expect(user.errors[:family_name]).to include('を入力してください')
+      end
+
+      it 'ユーザー本名(苗字)の不正値入力' do
+        user = build(:user, family_name: "ﾔﾏﾀﾞ")
+        user.valid?
+        expect(user.errors[:family_name]).to include('を全角で入力してください')
+      end
