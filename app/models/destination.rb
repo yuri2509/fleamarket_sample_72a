e536ea1ba989d403925840b26e06dd3c54125030
odattyan <62612364+odattyan@users.noreply.github.com>
2020-07-12 18:46:06 +0900

    validate追加

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -10,8 +10,10 @@ class Destination < ApplicationRecord
     format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
   validates :first_name_reading,
     format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
-
-  enum prefecture:{
+  validates :post_code,
+    format: { with: /\A\d{7}\z/, message: "をハイフン(-)無しで７桁の半角数字で入力してください" }
+  
+    enum prefecture:{
   北海道:1,青森県:2,岩手県:3,宮城県:4,秋田県:5,山形県:6,福島県:7,
   茨城県:8,栃木県:9,群馬県:10,埼玉県:11,千葉県:12,東京都:13,神奈川県:14,
   新潟県:15,富山県:16,石川県:17,福井県:18,山梨県:19,長野県:20,
@@ -21,5 +23,8 @@ class Destination < ApplicationRecord
   徳島県:36,香川県:37,愛媛県:38,高知県:39,
   福岡県:40,佐賀県:41,長崎県:42,熊本県:43,大分県:44,宮崎県:45,鹿児島県:46,沖縄県:47
   }
+
+  validates :prefecture,
+    format: {with: /\A(?!選択してください)/ , message: "を選択してください"}
 
 end
