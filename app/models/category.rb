823c50527faa39860a39860e5bea06fbb781163f
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-07-15 22:00:58 +0900

    バリデーションの設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  # validates :name, presence: true
+  validates :name, presence: true
 
   has_many :items
   has_ancestry
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ApplicationRecord
+  validates :src, presence: true
+
   mount_uploader :src, ImageUploader
   belongs_to :item
 end
