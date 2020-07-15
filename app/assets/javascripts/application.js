8524c703179ca97ead04f0f2bf257a2c7f364a85
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-07-15 15:51:24 +0900

    pry-railsの導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,6 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require turbolinks
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
