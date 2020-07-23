cad7c26e5b9d68921cc6bb3704487a126af70d55
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-07-23 22:59:19 +0900

    prefectureモデルの編集

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -18,4 +18,8 @@ class Prefecture < ActiveHash::Base
     {id: 43, name: '熊本県'}, {id: 44, name: '大分県'}, {id: 45, name: '宮崎県'},
     {id: 46, name: '鹿児島県'}, {id: 47, name: '沖縄県'}
   ]
+
+  include ActiveHash::Associations
+  has_many :items
+
 end
