327b25dae8a7498831f7394b378e657d3a04b5ec
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-08-01 20:38:30 +0900

    新規投稿商品一覧をビューに反映

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,5 @@
 class MessagesController < ApplicationController
   def index
-    # @items = Product.includes(:images).order('created_at DESC')
     @items_category = Item.includes(:images).order('created_at DESC').limit(4)
     @items_brand = Item.includes(:images).order('created_at DESC').limit(4).where.not(brand: "")
   end
