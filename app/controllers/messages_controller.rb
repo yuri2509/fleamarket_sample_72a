48cdb52fff34de0aae75280b0ee995ccab58b756
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-08-01 20:07:23 +0900

    ４つまで表示されるように編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,7 @@
 class MessagesController < ApplicationController
   def index
+    # @items = Product.includes(:images).order('created_at DESC')
+    @items_category = Item.includes(:images).order('created_at DESC').limit(4)
+    @items_brand = Item.includes(:images).order('created_at DESC').limit(4).where.not(brand: "")
   end
 end
