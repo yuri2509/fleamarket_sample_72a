50a68fe3e0411aae170457a2989cf5696076dce0
odattyan <62612364+odattyan@users.noreply.github.com>
2020-08-04 00:44:57 +0900

    モデル定義

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,5 @@
 class Card < ApplicationRecord
-  validates :user, :payjp :card_id, presence: true
+  validates :user, :payjp, :card_id, presence: true
   validates :user, :card_id, uniqueness: true
 
   belongs_to :user
