2ac0b7d431cee483937dc393be5f049f467feb15
odattyan <62612364+odattyan@users.noreply.github.com>
2020-08-09 02:17:13 +0900

    詳細機能

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -14,19 +14,46 @@ class CardsController < ApplicationController
     Payjp.api_key = 'sk_test_1be3aacbcbcc41ba0aaf616c'
 
     if params['payjpToken'].blank?
-      render "new", error: 'クレジットカードを登録してください'
+      redirect_to action: "new", error: 'クレジットカードを登録してください'
     else
       customer = Payjp::Customer.create(
-        description: 'test', 
         email: current_user.email,
         card: params["payjp-token"],
         metadata: {user_id: current_user.id }
       )
       @card =  Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to cards_path
+        redirect_to action: "show", id: current_user.id
       else
         render "new", error: 'クレジットカード情報が正しくありません'
+      end
+    end
+  end
+
+  def show
+    @card = Card.where(user_id: current_user.id).first
+    if @card.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = 'sk_test_1be3aacbcbcc41ba0aaf616c'
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+    end
+  end
+
+  def destroy
+    @card = Card.find_by(user_id: current_user.id)
+    if @card.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = 'sk_test_1be3aacbcbcc41ba0aaf616c'
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      customer.delete
+      @card.delete
+      if @card.destroy
+        
+      else
+        redirect_to card_path(current_user.id), error: "削除できませんでした。"
       end
     end
   end
