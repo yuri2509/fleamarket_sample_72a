e5e5e6819c93d0c811fe810da222c9c05011f1f3
odattyan <62612364+odattyan@users.noreply.github.com>
2020-08-09 21:40:24 +0900

    db変更

--- db/migrate/20200801101720_cards.rb
+++ db/migrate/20200801101720_cards.rb
@@ -2,7 +2,7 @@ class Cards < ActiveRecord::Migration[5.0]
   def change
     create_table :cards do |t|
       t.references :user,       null: false, unique: true
-      t.string :cutomer_id,          null: false
+      t.string :customer_id,          null: false
       t.string :card_id,       null: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,11 @@
 ActiveRecord::Schema.define(version: 20200801101720) do
 
   create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id",    null: false
-    t.string   "cutomer_id", null: false
-    t.string   "card_id",    null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "user_id",     null: false
+    t.string   "customer_id", null: false
+    t.string   "card_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.index ["user_id"], name: "index_cards_on_user_id", using: :btree
   end
 
