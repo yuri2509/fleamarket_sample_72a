03635f0e081ec35277b0b097ea23abaf76c9d7b1
odattyan <62612364+odattyan@users.noreply.github.com>
2020-08-11 19:11:55 +0900

    db作り直し

--- db/migrate/20200811100430_create_purchases.rb
+++ db/migrate/20200811100430_create_purchases.rb
@@ -1,4 +1,4 @@
-class Purchases < ActiveRecord::Migration[5.0]
+class CreatePurchases < ActiveRecord::Migration[5.0]
   def change
     create_table :purchases do |t|
       t.references :user,         null: false, foreign_key: true
