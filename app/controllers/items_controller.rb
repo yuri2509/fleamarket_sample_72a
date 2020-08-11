609771f57b90dceead9dcec3e83cf90e2de074f4
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-08-11 14:54:22 +0900

    ビューの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,7 @@ class ItemsController < ApplicationController
     if @item.update!(item_params)
       redirect_to root_path
     else
+      @item.images = []
       render action: :edit
     end
   end
