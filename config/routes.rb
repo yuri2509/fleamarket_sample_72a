9b3e4a142027477bf7916b485f7844af8d23e2b1
yuri2509 <gatx207-nicol@docomo.ne.jp>
2020-08-14 16:18:20 +0900

    routesの編集

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :cards, only: [:new, :create, :destroy, :show]
 
   root to: 'messages#index'
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+  resources :items
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
